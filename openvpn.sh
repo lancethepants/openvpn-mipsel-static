ed92d95fe6dc0e668b10133d16ea65104bb54bbb
lancethepants <lancethepants@gmail.com>
2015-09-23 11:36:26 -0600

    Update OpenSSL, lzo2, and OpenVPN to latest.

--- openvpn.sh
+++ openvpn.sh
@@ -42,28 +42,13 @@ make install DESTDIR=$BASE
 ########### #################################################################
 
 mkdir -p $SRC/openssl && cd $SRC/openssl
-$WGET http://www.openssl.org/source/openssl-1.0.1j.tar.gz
-tar zxvf openssl-1.0.1j.tar.gz
-cd openssl-1.0.1j
+$WGET https://www.openssl.org/source/openssl-1.0.2d.tar.gz
+tar zxvf openssl-1.0.2d.tar.gz
+cd openssl-1.0.2d
 
-cat << "EOF" > openssl.patch
---- Configure_orig      2013-11-19 11:32:38.755265691 -0700
-+++ Configure   2013-11-19 11:31:49.749650839 -0700
-@@ -402,6 +402,7 @@ my %table=(
- "linux-alpha+bwx-gcc","gcc:-O3 -DL_ENDIAN -DTERMIO::-D_REENTRANT::-ldl:SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_RISC1 DES_UNROLL:${alpha_asm}:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
- "linux-alpha-ccc","ccc:-fast -readonly_strings -DL_ENDIAN -DTERMIO::-D_REENTRANT:::SIXTY_FOUR_BIT_LONG RC4_CHUNK DES_INT DES_PTR DES_RISC1 DES_UNROLL:${alpha_asm}",
- "linux-alpha+bwx-ccc","ccc:-fast -readonly_strings -DL_ENDIAN -DTERMIO::-D_REENTRANT:::SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_INT DES_PTR DES_RISC1 DES_UNROLL:${alpha_asm}",
-+"linux-mipsel", "gcc:-DL_ENDIAN -DTERMIO -O3 -mtune=mips32 -mips32 -fomit-frame-pointer -Wall::-D_REENTRANT::-ldl:BN_LLONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL BF_PTR:${mips32_asm}:o32:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
-
- # Android: linux-* but without -DTERMIO and pointers to headers and libs.
- "android","gcc:-mandroid -I\$(ANDROID_DEV)/include -B\$(ANDROID_DEV)/lib -O3 -fomit-frame-pointer -Wall::-D_REENTRANT::-ldl:BN_LLONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL BF_PTR:${no_asm}:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
-EOF
-
-patch < openssl.patch
-
-./Configure linux-mipsel \
--ffunction-sections -fdata-sections -Wl,--gc-sections \
---prefix=/jffs shared zlib \
+./Configure linux-mips32 \
+-mtune=mips32 -mips32 -ffunction-sections -fdata-sections -Wl,--gc-sections \
+--prefix=/opts zlib \
 --with-zlib-lib=$DEST/lib \
 --with-zlib-include=$DEST/include
 
@@ -75,9 +60,9 @@ make CC=mipsel-linux-gcc install INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
 ######## ####################################################################
 
 mkdir $SRC/lzo2 && cd $SRC/lzo2
-$WGET http://www.oberhumer.com/opensource/lzo/download/lzo-2.08.tar.gz
-tar zxvf lzo-2.08.tar.gz
-cd lzo-2.08
+$WGET http://www.oberhumer.com/opensource/lzo/download/lzo-2.09.tar.gz
+tar zxvf lzo-2.09.tar.gz
+cd lzo-2.09
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS=$CPPFLAGS \
@@ -93,16 +78,17 @@ make install DESTDIR=$BASE
 ########### #################################################################
 
 mkdir $SRC/openvpn && cd $SRC/openvpn
-$WGET http://swupdate.openvpn.org/community/releases/openvpn-2.3.6.tar.gz
-tar zxvf openvpn-2.3.6.tar.gz
-cd openvpn-2.3.6
+$WGET http://swupdate.openvpn.org/community/releases/openvpn-2.3.8.tar.gz
+tar zxvf openvpn-2.3.8.tar.gz
+cd openvpn-2.3.8
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS=$CPPFLAGS \
 CFLAGS=$CFLAGS \
 CXXFLAGS=$CXXFLAGS \
 $CONFIGURE \
---disable-plugin-auth-pam
+--disable-plugin-auth-pam \
+--enable-password-save
 
 $MAKE LIBS="-all-static -lssl -lcrypto -lz -llzo2"
 make install DESTDIR=$BASE/openvpn
